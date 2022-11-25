55dd5d2ead2503ee5424885d95f227b5a6aa8e7a
NazarBezpaliuk <nazardxvx@gmail.com>
2022-11-25 10:39:11 +0200

    1

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modalRoot');
+
+export default function Modal({ onClose, pic }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        return onClose();
+      }
+    };
+
+    const handleClickAway = e => {
+      if (e.target.className.includes('Modal_overlay')) {
+        return onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('click', handleClickAway);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('click', handleClickAway);
+    };
+  });
+
+    return createPortal(
+      <div className={s.overlay}>
+        <div className={s.modal}>
+          <button
+            type="button"
+            className={s.modalCloseBtn}
+            onClick={onClose}
+          >
+            X
+          </button>
+          <img src={pic} alt="" />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  pic: PropTypes.string.isRequired,
+};
